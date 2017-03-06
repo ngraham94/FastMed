91212fb8d9f3a14dbae8098521082de13573301e
Nickolas Graham <n.graham94@gmail.com>
2017-03-06 15:23:57 -0500

    All changes so far

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { Component } from '@angular/core';
   template: `
   		<md-toolbar class="toolbar primary">FastMed</md-toolbar>
   		<div class="container-fluid">
-  			<md-icon class=col-lg-2 col-md-2 col-sm-6 col-xs-6>more_vert</md-icon>
+  			<md-icon class=col-lg-2 col-md-2 col-sm-2 col-xs-2>more_vert</md-icon>
+  			<div col-sm-8 col-xs-8 col-lg-hidden col-md-hidden></div>
   			<search-and-settings></search-and-settings>
-  			<md-icon class=col-lg-2 col-md-2 col-sm-6 col-xs-6>more_vert</md-icon>
+  			<md-icon class=col-lg-2 col-md-2 col-sm-2 col-xs-2>more_vert</md-icon>
   			<hospital-result></hospital-result>
   			<hospital-result></hospital-result>
 		</div>
